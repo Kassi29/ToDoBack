7aac6e4a06de339f6f38f10bb33f3092d967eb33
Kass Badani <kassbd29@gmail.com>
2024-11-18 21:45:20 -0400

    exceptions

---
+++ src/main/java/com/kass/backendtodo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.kass.backendtodo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(PersonalizedException.class)
+    public ResponseEntity<ErrorResponse> handleChangeParent(PersonalizedException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
